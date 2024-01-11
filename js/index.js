3a3b7f90b35911461ead8f35ea716757f30140ac
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-11 10:57:58 +0100

    Challenge completed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const dataObject = Object.fromEntries(formData);
+  console.log(dataObject);
+  const ageBadnessSum = 10 + Number(dataObject.age);
+  console.log(
+    `The age-badness-sum of ${dataObject.firstName} is ${ageBadnessSum}`
+  );
+
+  event.target.reset();
+
+  event.target.elements.firstName.focus();
+});
